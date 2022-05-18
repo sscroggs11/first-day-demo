0833f63a7451806eb92879702996cb1fbd86636c
Samuel Scroggs <102083372+sscroggs11@users.noreply.github.com>
2022-05-17 20:41:17 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,56 +1,106 @@
-var placeSearch, autocomplete, geocoder;
-
 function initAutocomplete() {
-  geocoder = new google.maps.Geocoder();
-  autocomplete = new google.maps.places.Autocomplete(
-    (document.getElementById('autocomplete')), {
-      types: ['geocode']
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -33.8688, lng: 151.2195 },
+    zoom: 13,
+    mapTypeId: "roadmap",
+  });
+  // Create the search box and link it to the UI element.
+  const input = document.getElementById("pac-input");
+  const searchBox = new google.maps.places.SearchBox(input);
+
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener("bounds_changed", () => {
+    searchBox.setBounds(map.getBounds());
+  });
+
+  let markers = [];
+
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener("places_changed", () => {
+    const places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+
+    // Clear out the old markers.
+    markers.forEach((marker) => {
+      marker.setMap(null);
     });
+    markers = [];
 
-  autocomplete.addListener('place_changed', fillInAddress);
+    // For each place, get the icon, name and location.
+    const bounds = new google.maps.LatLngBounds();
+
+    places.forEach((place) => {
+      if (!place.geometry || !place.geometry.location) {
+        console.log("Returned place contains no geometry");
+        return;
+      }
+
+      const icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25),
+      };
+
+      // Create a marker for each place.
+      markers.push(
+        new google.maps.Marker({
+          map,
+          icon,
+          title: place.name,
+          position: place.geometry.location,
+        })
+      );
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  });
 }
 
+window.initAutocomplete = initAutocomplete;
 
-function fillInAddress() {
-  var place = autocomplete.getPlace();
-
-  codeAddress(document.getElementById('autocomplete').value);
-}
-
-var x =document.getElementById('autocomplete').value
-
+var cityName="seattle"; //get from input
 var clientID= "nkyJu8Y-bgOpyEvKdevXhji09H6_azi1OlFzFM9W0-Y";
 
-window.addEventListener('load', loadImage(x));
+window.addEventListener('load', loadImage(cityName));
 
-function loadImage(x) {
-  var cityUrl = "https://api.unsplash.com/search/photos?query=" + x + "&page=1&per_page=1&client_id=" + clientID;
+function loadImage(cityName) {
+  var cityUrl = "https://api.unsplash.com/search/photos?query=" + cityName + "&page=1&per_page=1&client_id=" + clientID;
   console.log(cityUrl);
-  var imageEl = document.querySelector('.image');
     fetch(cityUrl)
         .then(function (response){
             return response.json();
         })
         .then(function(data) {
-            console.log(data);
-            var imageElement=document.createElement('img');
+            var imageElement = document.querySelector('#showimage');
             imageElement.src=data.results[0].urls.thumb;
-            imageEl.append(imageElement);
         });
 
 }
-// var cityName = "";  //To be pulled from either input or Sam's API after validation - should be pre-trimmed
+
+var cityName = "";  //To be pulled from either input or Sam's API after validation - should be pre-trimmed
 const wikiUsername = "arparent";  //Required for API - registered username for Adam Parent.
 var cityNameText = document.getElementById('city-name-text');       //Todo - Create this element in HTML.
 var wikiText = document.getElementById('wiki-text');                //Todo - Create this element in HTML.
 
-function getWikiApi(x) {                         //Takes the city name and returns a matching summary from Wikipedia.
+function getWikiApi(cityName) {                         //Takes the city name and returns a matching summary from Wikipedia.
     cityNameText.textContent = cityName;                //Display the city name on the page without alteration.
-    cityNameArr = x.split(" ");                  //Parse out any spaces in the city name - they cannot be in the URL.
+    cityNameArr = cityName.split(" ");                  //Parse out any spaces in the city name - they cannot be in the URL.
     wikiCityName = "";                                  //Creates a string for city name in the URL.
     for (var i = 0; i < cityNameArr.length; i++){       //Re-assembles the city name string, replacing spaces with "%20".
-        wikiCityName.concat(xArr[i]);
-        if((i + 1) < xArr.length)
+        wikiCityName.concat(cityNameArr[i]);
+        if((i + 1) < cityNameArr.length)
             wikiCityName.concat("%20");
         else
             break;
