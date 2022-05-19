136d407470589b7778be8c57f8edb98527540328
Samuel Scroggs <102083372+sscroggs11@users.noreply.github.com>
2022-05-18 18:50:08 -0700

    Add files via upload

---
+++ image_script.js
@@ -0,0 +1,23 @@
+/* Fetch API */
+var cityName="seattle"; //get from input
+var clientID= "nkyJu8Y-bgOpyEvKdevXhji09H6_azi1OlFzFM9W0-Y";
+
+window.addEventListener('load', loadImage(cityName));
+
+function loadImage(cityName) {
+  var cityUrl = "https://api.unsplash.com/search/photos?query=" + cityName + "&page=1&per_page=1&client_id=" + clientID;
+  console.log(cityUrl);
+    fetch(cityUrl)
+        .then(function (response){
+            return response.json();
+        })
+        .then(function(data) {
+            var imageElement = document.querySelector('#showimage');
+            imageElement.src=data.results[0].urls.thumb;
+        });
+
+}
+
+//"https://api.unsplash.com/search/photos?query=seattle&per_page=1&client_id=nkyJu8Y-bgOpyEvKdevXhji09H6_azi1OlFzFM9W0-Y
+
+//"https://api.unsplash.com/search/photos?query=coffee&per_page=20&client_id=nkyJu8Y-bgOpyEvKdevXhji09H6_azi1OlFzFM9W0-Y"
---
+++ wikiapi.js
@@ -0,0 +1,32 @@
+// wikiapi.js, created by Adam Parent, 10 May 2022.
+
+var cityName = "Salt Lake City";  //To be pulled from either input or Sam's API after validation - should be pre-trimmed
+const wikiUsername = "arparent";  //Required for API - registered username for Adam Parent.
+var cityNameText = document.getElementById('city-name-text');       //Todo - Create this element in HTML.
+var wikiText = document.getElementById('wiki-text');                //Todo - Create this element in HTML.
+
+function getWikiApi(cityName) {                         //Takes the city name and returns a matching summary from Wikipedia.
+    cityNameText.textContent = cityName;                //Display the city name on the page without alteration.
+    cityNameArr = cityName.split(" ");                  //Parse out any spaces in the city name - they cannot be in the URL.
+    var wikiCityName = "";                                   //Creates a string for city name in the URL.
+    for (var i = 0; i < cityNameArr.length; i++){       //Re-assembles the city name string, replacing spaces with "%20".
+        wikiCityName = wikiCityName.concat(cityNameArr[i]);
+        if((i + 1) < cityNameArr.length)
+            wikiCityName = wikiCityName.concat("%20");
+        else
+            break;
+    }
+    var wikiApiURL = "http://api.geonames.org/wikipediaSearchJSON?q=" + wikiCityName + "&maxRows=1&username=" + wikiUsername;   //Build the URL
+    fetch(wikiApiURL)
+      .then(function (response) {
+        if (response.status < 200 || response.status >= 400) {                  //Check Status - if not in 300 range, return error message.
+          wikiText.textContent = "Response Error Code: " + response.status;     //Todo - See if API returns any status messages, append if provided.
+        }                                                                       //Todo - If no data available, "summary not available" message instead.
+        return response.json();
+      })
+      .then(function (data) {
+        wikiText.textContent = data.geonames[0].summary;                           //If no error, populate text with result.
+      });
+}
+
+getWikiApi(cityName);
