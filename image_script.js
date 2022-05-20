9413679711d12f51d51faeedfbeec6fd21af8a8b
Samuel Scroggs <102083372+sscroggs11@users.noreply.github.com>
2022-05-19 19:52:23 -0700

    Update image_script.js

--- image_script.js
+++ image_script.js
@@ -1,5 +1,5 @@
 /* Fetch API */
-var cityName=document.getElementsByID("pac-input").value; //get from input
+var cityName=document.getElementByID("pac-input").value; //get from input
 var clientID= "nkyJu8Y-bgOpyEvKdevXhji09H6_azi1OlFzFM9W0-Y";
 
 window.addEventListener('load', loadImage(cityName));
